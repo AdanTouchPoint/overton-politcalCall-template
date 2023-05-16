8e5160ad381dda324391b543c29b9cacb3f42dd3
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-05-15 19:16:09 -0500

    fix share component

--- app/components/Share.js
+++ app/components/Share.js
@@ -1,19 +1,19 @@
 import Head from "next/head";
 import { LinkedinIcon, LinkedinShareButton,FacebookShareButton, TwitterShareButton,WhatsappShareButton,FacebookIcon, TwitterIcon, WhatsappIcon } from "react-share";
 import "./share.css";
-const Share = ({shareMessage,shareUrl}) => {  
+const Share = ({shareMessage,shareUrl }) => {  
   return (
     <div >
-      <FacebookShareButton url={shareUrl} quote={shareMessage.replace(/"/g, '&quot;')} hashtag="#yodecidocomomemuevo" className="share-buttons">
+      <FacebookShareButton url={shareUrl? shareUrl: 'Fill this in your dashboard'} quote={shareMessage? shareMessage.replace(/"/g, '&quot;') :  'Fill this in your dashboard'} hashtag="#yodecidocomomemuevo" className="share-buttons">
       <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
-      <TwitterShareButton url={shareUrl} title={shareMessage}className="share-buttons">
+      <TwitterShareButton url={shareUrl? shareUrl: 'Fill this in your dashboard'} title={shareMessage? shareMessage : 'Fill this in your dashboard'}className="share-buttons">
       <TwitterIcon size={32} round={true} />
       </TwitterShareButton>
-      <WhatsappShareButton url={shareUrl} title={shareMessage} className="share-buttons">
+      <WhatsappShareButton url={shareUrl? shareUrl: 'Fill this in your dashboard'} title={shareMessage? shareMessage : 'Fill this in your dashboard'} className="share-buttons">
       <WhatsappIcon size={32} round={true} />
       </WhatsappShareButton>
-      <LinkedinShareButton url={shareUrl} title={shareMessage} className="share-buttons">
+      <LinkedinShareButton url={shareUrl? shareUrl: 'Fill this in your dashboard'} title={shareMessage? shareMessage : 'Fill this in your dashboard'} className="share-buttons">
       <LinkedinIcon size={32} round={true} />
       </LinkedinShareButton>
       <Head>
